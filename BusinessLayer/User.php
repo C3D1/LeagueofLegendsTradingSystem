40c0f3494556cf8a0fb6953f7ef20327435d803b
Ipod12321 <gpioloco@gmail.com>
2016-06-01 11:20:14 +0200

    ewwc
    e

--- BusinessLayer/User.php
+++ BusinessLayer/User.php
@@ -1,1 +1,13 @@
-User.php
+<?php 
+	$db = odbc_pconnect("LolTrade2","root","") or die("Datenbankverbindung ist fehlgeschlagen x");
+
+	$query = "Get * FROM user";
+	$result = odbc_exec($db,$query);
+
+	while($row= odbc_fetch_array($result)) {
+		echo($row["username"]);
+		echo($row["password"]);
+	}
+
+ ?>
+
