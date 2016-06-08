2fa532e1bf57b8d808d85cfa3d462539775d256f
C3D1 <cedric_schnider@outlook.com>
2016-06-08 11:06:27 +0200

    Grunddesign hinzugefügt

--- PresentationLayer/Index.php
+++ PresentationLayer/Index.php
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-=======
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,9 +6,18 @@
 	<link rel="stylesheet" type="text/css" href="DesignLogin.css">
 </head>
 <body>
-
+	<div class="header">
+		<H1>Trading System</H1>
+		<img src="../Images/LoL_Icon.png" alt="League of Legends Icon" class="icon">
+	</div>
+	<div class="content">
+		
+	</div>
+	<div class="footer">
+		
+	</div>
 </body>
 </html>
 <?php 
  ?>
->>>>>>> origin/master
+
