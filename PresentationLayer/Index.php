d6772120cda727a1c9c437e1d832b54c4fdd098b
C3D1 <cedric_schnider@outlook.com>
2016-06-22 09:29:07 +0200

    Dreckige Scheiss korrigiert

--- PresentationLayer/Index.php
+++ PresentationLayer/Index.php
@@ -9,16 +9,6 @@
 	<div class="header">
 		<H1>Trading System</H1>
 		<img src="../Images/LoL_Icon.png" alt="League of Legends Icon" class="icon">
-		<div class="navi">
-			
-		</div>
-		<?php 
-		if($_SESSION["login"] != 12345)
-		{
-			echo("<button><a href=\"Login.php\">Logout</a></button>");
-		}
-		
-		 ?>
 		
 	</div>
 	<div class="content">
@@ -27,17 +17,16 @@
 		    <li><a href="#">Trade</a></li>
 		    <li><a href="#">Request</a></li>
 		</ul>
+		<?php 
+			if($_SESSION["login"] != 12345)
+			{
+				echo("<button><a href=\"Login.php\">Logout</a></button>");
+			}
+		
+		 ?>
 	</div>
 	<div class="footer">
 		<p>&copy; Cédric Schnider, Gino Pio Loco<p>
 	</div>
 </body>
 </html>
-<?php 
- ?>
-
-<button><a href="Login.php">Login</a></button>
-</body>
-</html>
-<?php 
- ?>
